0801f727e08658fa3665260ea6fb57762e157886
Pauldarlefelbacq <paul.darle-felbacq@edu.univ-fcomte.fr>
2026-03-23 14:38:09 +0100

    migrations pour les commentaires (tests)

---
+++ pocketbase/pb_migrations/1774267241_updated_Commentaires_coach.js
@@ -0,0 +1,20 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2660456165")
+
+  // update collection data
+  unmarshal({
+    "createRule": "@request.auth.id != \"\" && @request.body.user = @request.auth.id && @request.auth.coach != true && @request.auth.id ?!~ @collection.Commentaires_coach.user.id"
+  }, collection)
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2660456165")
+
+  // update collection data
+  unmarshal({
+    "createRule": "@request.auth.id != \"\" && @request.body.user = @request.auth.id && @request.auth.coach != true && @request.auth.id ?= @collection.Commentaires_coach.user.id"
+  }, collection)
+
+  return app.save(collection)
+})
