fdae998efb8c0db15594032bb89a24df396b30f6
Pauldarlefelbacq <paul.darle-felbacq@edu.univ-fcomte.fr>
2026-03-23 08:29:49 +0100

    préparation pour tous les procains problèmes de déploiement

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  security: {
+    checkOrigin: false
+  },
+  image: {
+    domains: ['r414-recettes-cuisine.pauldarlef.fr']
+  },
   vite: {
     plugins: [tailwindcss()]
   }
